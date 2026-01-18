66fd7c24d95ef8c43f905d7820331e3b4954cc65
João Nodari <joaonodari2017@hotmail.com>
2026-01-18 19:50:15 -0300

    Implementação do sistema de perfil .

--- pinterestpython/routes.py
+++ pinterestpython/routes.py
@@ -1,13 +1,19 @@
 # Criar as rotas do nosso site.
 from flask import render_template, url_for, redirect
 from pinterestpython import app, database, bcrypt
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from pinterestpython.forms import formLogin, formCriarConta
 from pinterestpython.models import Usuario, Foto
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     form_Login = formLogin()
+    if form_Login.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=form_Login.email.data).first()
+        if usuario:
+            bcrypt.check_password_hash(usuario.senha, form_Login.senha.data)
+            login_user(usuario)
+            return redirect(url_for("perfil", id_usuario=usuario.id))
     return render_template('html/homepage.html', form=form_Login)
 
 @app.route("/criarconta", methods=["GET", "POST"])
@@ -20,10 +26,21 @@ def criarConta():
         database.session.add(usuario)
         database.session.commit()
         login_user(usuario, remember=True)
-        return redirect(url_for("perfil", usuario=usuario.username))
+        return redirect(url_for("perfil", id_usuario=usuario.id))
     return render_template('html/criarConta.html', form=form_CriarConta)
 
-@app.route("/perfil/<usuario>")
+@app.route("/perfil/<id_usuario>")
 @login_required
-def perfil(usuario):
-    return render_template('html/perfil.html', usuario=usuario)
+def perfil(id_usuario):
+    if int(id_usuario) == int(current_user.id):
+        # O usuario ta vendo o perfil dele
+        return render_template("perfil.html", usuario=current_user)
+    else:
+        usuario = Usuario.query.get(int(id_usuario))
+        return render_template('html/perfil.html', usuario=usuario)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("homepage"))
